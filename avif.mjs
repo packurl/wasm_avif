c59ad07c7831f415a38f0eae981ee7ba2d824386
programingjd <programingjd@gmail.com>
2023-06-25 15:16:16 +0200

    Dependencies and js wrapper update.

--- avif.mjs
+++ avif.mjs
@@ -4,7 +4,7 @@ const src=()=>`(async()=>{
   const mod=await WebAssembly.compileStreaming(await fetch('${url}',{cache:'force-cache'}));
   const imports={
     wbg:{
-      __wbg_log_d5989ac76b02a61f: (p,n)=>{
+      __wbg_log_d415f1170b035824: (p,n)=>{
         console.log(
           new TextDecoder().decode(new Uint8Array(wasm.memory.buffer).subarray(p,p+n))
         )
@@ -12,11 +12,14 @@ const src=()=>`(async()=>{
     }
   };
   const wasm=(await WebAssembly.instantiate(mod,imports)).exports;
-  const malloc=wasm.__wbindgen_malloc;const free=wasm.__wbindgen_free;
+  const malloc=wasm.__wbindgen_malloc;
+  const free=wasm.__wbindgen_free;
+  const pointer=wasm.__wbindgen_add_to_stack_pointer;
   const fn=({data,width,height,quality,speed})=>{
+    const n1=data.length;
+    const p1=malloc(n1,1);
+    const r=pointer(-16);
     try{
-      const r=wasm.__wbindgen_add_to_stack_pointer(-16);
-      const n1=data.length;const p1=malloc(n1);
       new Uint8Array(wasm.memory.buffer).set(data,p1);
       wasm.avif_from_imagedata(r,p1,n1,width,height,quality,speed);
       const arr=new Int32Array(wasm.memory.buffer);
@@ -24,9 +27,7 @@ const src=()=>`(async()=>{
       const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
       free(p2,n2);
       return res;
-    }finally{
-      wasm.__wbindgen_add_to_stack_pointer(16);
-    }
+    }finally{pointer(16)}
   };
   onmessage=async msg=>postMessage(fn(msg.data));
   postMessage('ready');
