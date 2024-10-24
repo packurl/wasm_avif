9e6066d41cae4734006d217c5bd01203112e40a9
programingjd <programingjd@gmail.com>
2024-10-24 23:43:19 +0200

    Dependency update. Scripts update.

--- avif.mjs
+++ avif.mjs
@@ -1,15 +1,25 @@
 const url=new URL('avif.wasm',import.meta.url);
+let wasm;
 const imports={
   wbg: {
-    __wbg_log_12edb8942696c207: (p,n)=>{
+    __wbg_log_aa11a1e72555df69: (p,n)=>{
       console.log(new TextDecoder().decode(new Uint8Array(wasm.memory.buffer).subarray(p,p+n)));
+    },
+    __wbindgen_init_externref_table:function(){
+      const table=wasm.__wbindgen_export_0;
+      const offset=table.grow(4);
+      table.set(0);
+      table.set(offset);
+      table.set(offset+1,null);
+      table.set(offset+2,true);
+      table.set(offset+3,false);
     }
   }
 };
-const {instance: {exports: wasm}}=await WebAssembly.instantiateStreaming(await fetch(url,{cache: 'force-cache'}),imports);
+const {instance}=await WebAssembly.instantiateStreaming(await fetch(url,{cache: 'force-cache'}),imports);
+wasm=instance.exports;
 const malloc=wasm.__wbindgen_malloc;
 const free=wasm.__wbindgen_free;
-const pointer=wasm.__wbindgen_add_to_stack_pointer;
 /**
  * Encodes the supplied ImageData rgba array.
  * @param {Uint8Array} bytes
@@ -22,19 +32,11 @@ const pointer=wasm.__wbindgen_add_to_stack_pointer;
 const avif=(bytes,width,height,quality=50,speed=6)=>{
   const n1=bytes.length;
   const p1=malloc(n1,1);
-  const r=pointer(-16);
-  try{
-    new Uint8Array(wasm.memory.buffer).set(bytes,p1);
-    wasm.avif_from_imagedata(r,p1,n1,width,height,quality,speed);
-    const arr=new Int32Array(wasm.memory.buffer);
-    const p2=arr[r/4];
-    const n2=arr[r/4+1];
-    const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
-    free(p2,n2);
-    return res;
-  }finally{
-    pointer(16)
-  }
+  new Uint8Array(wasm.memory.buffer).set(bytes,p1);
+  const [p2,n2]=wasm.avif_from_imagedata(p1,n1,width,height,quality,speed);
+  const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
+  free(p2,n2);
+  return res;
 };
 export {avif};
 export default avif;
--- avif_for_importScripts.js
+++ avif_for_importScripts.js
@@ -1,30 +1,32 @@
 const avif=(async()=>{
+  let wasm;
   const imports={
     wbg: {
-      __wbg_log_12edb8942696c207: (p,n)=>{
+      __wbg_log_aa11a1e72555df69: (p,n)=>{
         console.log(new TextDecoder().decode(new Uint8Array(wasm.memory.buffer).subarray(p,p+n)));
+      },
+      __wbindgen_init_externref_table:function(){
+        const table=wasm.__wbindgen_export_0;
+        const offset=table.grow(4);
+        table.set(0);
+        table.set(offset);
+        table.set(offset+1,null);
+        table.set(offset+2,true);
+        table.set(offset+3,false);
       }
     }
   };
-  const {instance: {exports: wasm}}=await WebAssembly.instantiateStreaming(await fetch('./avif.wasm',{cache: 'force-cache'}),imports);
+  const {instance}=await WebAssembly.instantiateStreaming(await fetch('./avif.wasm',{cache: 'force-cache'}),imports);
+  wasm=instance.exports;
   const malloc=wasm.__wbindgen_malloc;
   const free=wasm.__wbindgen_free;
-  const pointer=wasm.__wbindgen_add_to_stack_pointer;
   return (it,width,height,quality=50,speed=6)=>{
     const n1=it.length;
     const p1=malloc(n1,1);
-    const r=pointer(-16);
-    try{
-      new Uint8Array(wasm.memory.buffer).set(data,p1);
-      wasm.avif_from_imagedata(r,p1,n1,width,height,quality,speed);
-      const arr=new Int32Array(wasm.memory.buffer);
-      const p2=arr[r/4];
-      const n2=arr[r/4+1];
-      const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
-      free(p2,n2);
-      return res;
-    }finally{
-      pointer(16)
-    }
+    new Uint8Array(wasm.memory.buffer).set(data,p1);
+    const [p2,n2]=wasm.avif_from_imagedata(p1,n1,width,height,quality,speed);
+    const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
+    free(p2,n2);
+    return res;
   };
 })();
